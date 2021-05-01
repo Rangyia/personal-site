7f72d23ad15018611c18b6545f4a42c0c9687d7e
rangyia <jess.graham.dev@gmail.com>
2021-05-01 01:12:36 -0400

    Add routes for new pages

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppBar } from '@material-ui/core';
+
+export default function NavBar() {
+    return (
+        <AppBar>
+
+        </AppBar>
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+
+export const Routes = {
+    // Pages
+    Home: { path: "/" },
+
+    // Views
+    Frontene: { path: "/frontend" },
+    Backend: { path: "/backend" },
+    Hacking: { path: "/hacking" },
+    Devops: { path: "/devops" },
+}
